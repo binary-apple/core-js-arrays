e23c054685c734f7f2c695dc2be3cf2f496303a7
Anna Perevedentseva <anna.pereved@gmail.com>
2024-12-08 16:49:22 +0100

    feat: implement shiftArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -545,8 +545,11 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const shiftSize = n >= 0 ? n : arr.length + n;
+  return arr
+    .slice(arr.length - shiftSize)
+    .concat(arr.slice(0, arr.length - shiftSize));
 }
 
 /**
