ece2d334f193a6e3b55baec5c31f4c0e639b8602
Anna Perevedentseva <anna.pereved@gmail.com>
2024-12-08 17:10:03 +0100

    feat: implement sortDigitNamesByNumericOrder

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -565,8 +565,22 @@ function shiftArray(arr, n) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  return arr.sort((a, b) => {
+    const digits = [
+      'zero',
+      'one',
+      'two',
+      'three',
+      'four',
+      'five',
+      'six',
+      'seven',
+      'eight',
+      'nine',
+    ];
+    return digits.indexOf(a) - digits.indexOf(b);
+  });
 }
 
 /**
